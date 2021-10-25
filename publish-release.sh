b03dd5eb292f768c9be3fd68a0a14a2473f934ed
behring <behring_zhao@126.com>
2021-10-25 15:59:15 +0800

    更新发布脚本

--- publish-release.sh
+++ publish-release.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+./gradlew build
 git tag -m "支持合约文本生成" v"$1"
 git push origin v"$1"
 gh release create v"$1" ./build/libs/dark-horse-tools-"$1".jar -F changelogs/changelog-"$1".md
