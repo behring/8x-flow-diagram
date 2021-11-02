47dba9e2cfb83440bb0946947e2632c0c8f5ad14
behring <behring.zhao@gmail.com>
2021-11-02 22:44:08 +0800

    设置关联关系线条颜色

--- src/main/kotlin/doxflow/models/diagram/Relationship.kt
+++ src/main/kotlin/doxflow/models/diagram/Relationship.kt
@@ -2,11 +2,12 @@ package doxflow.models.diagram
 
 interface Relationship {
     companion object {
-        const val ONE_TO_N = """ "1" -- "N" """
-        const val ONE_TO_ONE = """ "1" -- "1" """
-        const val N_TO_N = """ "N" -- "N" """
-        const val NONE = """ -- """
-        const val PLAY_TO = """ ..|> """
-        const val ASSOCIATE = """ --> """
+        private const val lineColor = "[#000000]"
+        const val ONE_TO_N = """ "1" -$lineColor- "N" """
+        const val ONE_TO_ONE = """ "1" -$lineColor- "1" """
+        const val N_TO_N = """ "N" -$lineColor- "N" """
+        const val NONE = """ -$lineColor- """
+        const val PLAY_TO = """ .$lineColor.|> """
+        const val ASSOCIATE = """ -$lineColor-> """
     }
 }
