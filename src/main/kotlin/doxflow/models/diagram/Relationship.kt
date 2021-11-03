e4d30d6479df5b2cacea113bba5d24104667886e
behring <behring.zhao@gmail.com>
2021-11-03 11:08:37 +0800

    优化关联关系,增加跨上下文关联关系

--- src/main/kotlin/doxflow/models/diagram/Relationship.kt
+++ src/main/kotlin/doxflow/models/diagram/Relationship.kt
@@ -3,9 +3,11 @@ package doxflow.models.diagram
 interface Relationship {
     companion object {
         private const val lineColor = "[#000000]"
-        const val ONE_TO_N = """ "1" -$lineColor- "N" """
-        const val ONE_TO_ONE = """ "1" -$lineColor- "1" """
         const val NONE = """ -$lineColor- """
+        const val ONE_TO_ONE = """ "1" -$lineColor- "1" """
+        const val ONE_TO_N = """ "1" -$lineColor- "N" """
+        const val ONE_TO_ONE_OVER_CONTEXT = """ "1" .$lineColor. "1" """
+        const val ONE_TO_N_OVER_CONTEXT = """ "1" .$lineColor. "N" """
         const val PLAY_TO = """ .$lineColor.> """
         const val DEFAULT = ONE_TO_ONE
     }
